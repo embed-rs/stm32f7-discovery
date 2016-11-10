b62a6209e519d81ea9a70eb74dfc9bebc8130c8a
Philipp Oppermann <dev@phil-opp.com>
2016-11-10 14:48:47 +0100

    Link rlibc and compiler_builtins
    rlibc is needed for memcpy, memmove, etc. and compiler_builtins is needed for `__aeabi_memcpy4` etc.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@
 #![no_std]
 #![no_main]
 
+// various compiler builtins such as `__aeabi_memcpy4`
+extern crate compiler_builtins_snapshot;
+// memcpy, memmove, etc. This needs to be below the compiler_builtins line, otherwise a linker
+// error occurs (TODO: why?)
+extern crate rlibc;
+
 #[no_mangle]
 pub unsafe extern "C" fn reset() -> ! {
     main();
