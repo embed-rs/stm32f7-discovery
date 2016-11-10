18618b4c63c5faa94f9882ddb02af7ed27a89bbe
Philipp Oppermann <dev@phil-opp.com>
2016-11-10 14:52:33 +0100

    Use the r0 crate to initialize .data and .bss
    We need to copy the .data contents from flash to RAM and we need to zero the .bss section. We need to do both things on every reset.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,32 @@ extern crate compiler_builtins_snapshot;
 // memcpy, memmove, etc. This needs to be below the compiler_builtins line, otherwise a linker
 // error occurs (TODO: why?)
 extern crate rlibc;
+// initialization routines for .data and .bss
+extern crate r0;
 
 #[no_mangle]
 pub unsafe extern "C" fn reset() -> ! {
+    extern "C" {
+        static __DATA_LOAD: u32;
+        static __DATA_END: u32;
+        static mut __DATA_START: u32;
+
+        static mut __BSS_START: u32;
+        static mut __BSS_END: u32;
+    }
+
+    let data_load = &__DATA_LOAD;
+    let data_start = &mut __DATA_START;
+    let data_end = &__DATA_END;
+
+    let bss_start = &mut __BSS_START;
+    let bss_end = &__BSS_END;
+
+    // initializes the .data section (Copy the data segment initializers from flash to SRAM)
+    r0::init_data(data_start, data_end, data_load);
+    // zeroes the .bss section
+    r0::zero_bss(bss_start, bss_end);
+
     main();
 }
 
