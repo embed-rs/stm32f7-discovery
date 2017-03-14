c8d9949519fe745793e6f6b6e385326a90f36df8
kathrinbr <Kathrin.Reibelt@kit.edu>
2017-03-14 16:10:27 +0100

    Update gdb.bat

--- gdb.bat
+++ gdb.bat
@@ -1,4 +1,4 @@
-echo "Please run 'st-util' in another terminal window (you might need sudo)"
+echo "Please run 'st-util' in another terminal window"
 echo ""
 
 arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" target/stm32f7/debug/novemb-rs-stm32f7
