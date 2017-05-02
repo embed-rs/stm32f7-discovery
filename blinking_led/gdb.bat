900340ee8b704c56c1ac9e8157991468e7295917
Fabio B <f-bro@mailbox.org>
2017-05-02 15:03:28 +0200

    Add gdb.bat to blinking_led example

---
+++ blinking_led/gdb.bat
@@ -0,0 +1,4 @@
+echo "Please run 'st-util' in another terminal window"
+echo ""
+
+arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" target/stm32f7/debug/blinking_led
