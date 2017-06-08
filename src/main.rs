25258a564ea0051e33b27e368a64dd98931f0046
Jonathan Pallant <github@thejpster.org.uk>
2017-06-08 20:04:07 +0100

    Remove intermediate variables from init.

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,17 @@ use collections::borrow::Cow;
 pub unsafe extern "C" fn reset() -> ! {
     extern "C" {
         static __DATA_LOAD: u32;
-        static __DATA_END: u32;
+        static mut __DATA_END: u32;
         static mut __DATA_START: u32;
 
         static mut __BSS_START: u32;
         static mut __BSS_END: u32;
     }
 
-    let data_load = &__DATA_LOAD;
-    let data_start = &mut __DATA_START;
-    let data_end = &__DATA_END;
-
-    let bss_start = &mut __BSS_START;
-    let bss_end = &__BSS_END;
-
     // initializes the .data section (copy the data segment initializers from flash to RAM)
-    r0::init_data(data_start, data_end, data_load);
+    r0::init_data(&mut __DATA_START, &mut __DATA_END, &__DATA_LOAD);
     // zeroes the .bss section
-    r0::zero_bss(bss_start, bss_end);
+    r0::zero_bss(&mut __BSS_START, &__BSS_END);
 
     stm32f7::heap::init();
 
