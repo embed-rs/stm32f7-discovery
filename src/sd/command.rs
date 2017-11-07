b70dad57f2f8c75e743fce2b14b20a192c42caec
flip1995 <uwdkn@student.kit.edu>
2017-11-07 13:32:31 +0100

    Finishing initialization of the sd card.

--- src/sd/command.rs
+++ src/sd/command.rs
@@ -1,12 +1,13 @@
 use super::error::*;
 use board::sdmmc::Sdmmc;
 
-pub fn send_command_idle(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
-    send_command(sdmmc, 0, 0x00, true, false, false, 0x03, false);
+/// Set the SD card into idle state
+pub fn send_cmd_idle(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
+    send_cmd(sdmmc, 0, 0x00, true, false, false, 0x00, false);
 
-    let mut timeout = ::system_clock::ticks() as u32 + timeout;
+    let timeout = ::system_clock::ticks() as u32 + timeout;
     while (::system_clock::ticks() as u32) < timeout
-        && !sdmmc.sta.read().cmdsent() { timeout -=1; }
+        && !sdmmc.sta.read().cmdsent() {}
 
     if timeout == 0 {
         return Err(Error::Timeout);
@@ -15,14 +16,27 @@ pub fn send_command_idle(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn send_command_app(sdmmc: &mut Sdmmc, argument: u32) -> Result<u32, Error> {
-    send_command(sdmmc, argument, 55, true, false, false, 0x01, false);
+/// Send CMD55 to signalize that the next command is an app command
+pub fn send_cmd_app(sdmmc: &mut Sdmmc, argument: u32) -> Result<u32, Error> {
+    send_cmd(sdmmc, argument, 55, true, false, false, 0x01, false);
 
     get_cmd_resp1(sdmmc, 55, 5000)
 }
 
-pub fn send_command_oper_cond(sdmmc: &mut Sdmmc) -> Result<(), Error> {
-    send_command(sdmmc, 0x1AA, 8, true, false, false, 0x01, false);
+/// Send ACMD41 to get the operation condition register (OCR) of the card.
+/// Always send CMD55 before sending this command.
+pub fn send_cmd_app_oper(sdmmc: &mut Sdmmc, capacity: u32) -> Result<(), Error> {
+    send_cmd(sdmmc, 0x8010_0000 | capacity, 41, true, false, false, 0x01, false);
+
+    get_cmd_resp3(sdmmc, 5000)?;
+
+    Ok(())
+}
+
+/// Get the Operation Condition of the card. This command is only supported
+/// by SD card v2 and can therefore be used to determine the version of the card.
+pub fn send_cmd_oper_cond(sdmmc: &mut Sdmmc) -> Result<(), Error> {
+    send_cmd(sdmmc, 0x1AA, 8, true, false, false, 0x01, false);
 
     wait_resp(sdmmc, 5000)?;
 
@@ -31,7 +45,25 @@ pub fn send_command_oper_cond(sdmmc: &mut Sdmmc) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn send_command(sdmmc: &mut Sdmmc,
+/// Get the Card Indentification Number (CID) of the card. (CMD2)
+pub fn send_cmd_send_cid(sdmmc: &mut Sdmmc) -> Result<(), Error> {
+    send_cmd(sdmmc, 0, 2, true, false, false, 0x03, false);
+
+    get_cmd_resp2(sdmmc, 5000)?;
+
+    Ok(())
+}
+
+/// Get the Relative Card Address (RCA) of the card. This number is shorter
+/// than the CID. (CMD3)
+pub fn send_cmd_set_rel_add(sdmmc: &mut Sdmmc) -> Result<u16, Error> {
+    send_cmd(sdmmc, 0, 3, true, false, false, 0x01, false);
+
+    get_cmd_resp6(sdmmc, 3, 5000)
+}
+
+/// Send a command to the card.
+pub fn send_cmd(sdmmc: &mut Sdmmc,
                 argument: u32, cmdidx: u8,
                 cpsmen: bool,
                 waitpend: bool, waitint: bool, waitresp: u8,
@@ -82,12 +114,38 @@ fn get_cmd_resp3(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
     Ok(())
 }
 
+fn get_cmd_resp6(sdmmc: &mut Sdmmc, cmd_idx: u8, timeout: u32) -> Result<u16, Error> {
+    wait_resp_crc(sdmmc, timeout)?;
+
+    // TODO: always 0?!
+    if sdmmc.respcmd.read().respcmd() != cmd_idx {
+        return Err(Error::SdmmcError {
+            t: SdmmcErrorType::CmdCrcFailed
+        });
+    }
+
+    clear_all_static_status_flags(sdmmc);
+
+    // Get response and check card status for errors
+    let card_status = sdmmc.resp1.read().cardstatus1();
+
+    if card_status & (R6_CRC_FAILED | R6_ILLEGAL_COMMAND | R6_GENERAL_UNKNOWN_ERROR).bits() == 0 {
+        Ok((card_status >> 16) as u16)
+    } else if card_status & R6_CRC_FAILED.bits() != 0 {
+        Err(Error::CardError { t: R6_CRC_FAILED })
+    } else if card_status & R6_ILLEGAL_COMMAND.bits() != 0 {
+        Err(Error::CardError { t: R6_ILLEGAL_COMMAND })
+    } else {
+        Err(Error::CardError { t: R6_GENERAL_UNKNOWN_ERROR })
+    }
+}
+
 fn wait_resp(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
-    let mut timeout = ::system_clock::ticks() as u32 + timeout;
+    let timeout = ::system_clock::ticks() as u32 + timeout;
     while (::system_clock::ticks() as u32) < timeout
         && !sdmmc.sta.read().cmdrend()
         && !sdmmc.sta.read().ccrcfail()
-        && !sdmmc.sta.read().ctimeout() { timeout -=1; }
+        && !sdmmc.sta.read().ctimeout() {}
 
     if timeout == 0 {
         return Err(Error::Timeout);
@@ -115,7 +173,6 @@ fn wait_resp_crc(sdmmc: &mut Sdmmc, timeout: u32) -> Result<(), Error> {
     Ok(())
 }
 
-//TODO:move?
 fn clear_all_static_status_flags(sdmmc: &mut Sdmmc) {
     sdmmc.icr.update(|icr| {
         icr.set_ccrcfailc(true);
@@ -133,42 +190,40 @@ fn clear_all_static_status_flags(sdmmc: &mut Sdmmc) {
 
 fn check_for_errors(card_status: u32) -> Result<(), Error> {
     if card_status & OCR_ERROR_BITS.bits() == 0 {
-        return Ok(())
+        Ok(())
     } else if card_status & AKE_SEQ_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: AKE_SEQ_ERROR });
+        Err(Error::CardError { t: AKE_SEQ_ERROR })
     } else if card_status & ERASE_RESET.bits() != 0 {
-        return Err(Error::CardError { t: ERASE_RESET });
+        Err(Error::CardError { t: ERASE_RESET })
     } else if card_status & CARD_ECC_DISABLED.bits() != 0 {
-        return Err(Error::CardError { t: CARD_ECC_DISABLED });
+        Err(Error::CardError { t: CARD_ECC_DISABLED })
     } else if card_status & WP_ERASE_SKIP.bits() != 0 {
-        return Err(Error::CardError { t: WP_ERASE_SKIP });
+        Err(Error::CardError { t: WP_ERASE_SKIP })
     } else if card_status & CID_CSD_OVERWRITE.bits() != 0 {
-        return Err(Error::CardError { t: CID_CSD_OVERWRITE });
+        Err(Error::CardError { t: CID_CSD_OVERWRITE })
     } else if card_status & CC_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: CC_ERROR });
+        Err(Error::CardError { t: CC_ERROR })
     } else if card_status & CARD_ECC_FAILED.bits() != 0 {
-        return Err(Error::CardError { t: CARD_ECC_FAILED });
+        Err(Error::CardError { t: CARD_ECC_FAILED })
     } else if card_status & ILLEGAL_COMMAND.bits() != 0 {
-        return Err(Error::CardError { t: ILLEGAL_COMMAND });
+        Err(Error::CardError { t: ILLEGAL_COMMAND })
     } else if card_status & COM_CRC_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: COM_CRC_ERROR });
+        Err(Error::CardError { t: COM_CRC_ERROR })
     } else if card_status & LOCK_UNLOCK_FAILED.bits() != 0 {
-        return Err(Error::CardError { t: LOCK_UNLOCK_FAILED });
+        Err(Error::CardError { t: LOCK_UNLOCK_FAILED })
     } else if card_status & WP_VIOLATION.bits() != 0 {
-        return Err(Error::CardError { t: WP_VIOLATION });
+        Err(Error::CardError { t: WP_VIOLATION })
     } else if card_status & ERASE_PARAM.bits() != 0 {
-        return Err(Error::CardError { t: ERASE_PARAM });
+        Err(Error::CardError { t: ERASE_PARAM })
     } else if card_status & ERASE_SEQ_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: ERASE_SEQ_ERROR });
-    } else if card_status & AKE_SEQ_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: AKE_SEQ_ERROR });
+        Err(Error::CardError { t: ERASE_SEQ_ERROR })
     } else if card_status & BLOCK_LEN_ERROR.bits() != 0 {
-        return Err(Error::CardError { t: BLOCK_LEN_ERROR });
+        Err(Error::CardError { t: BLOCK_LEN_ERROR })
     } else if card_status & ADDRESS_MISALIGNED.bits() != 0 {
-        return Err(Error::CardError { t: ADDRESS_MISALIGNED });
+        Err(Error::CardError { t: ADDRESS_MISALIGNED })
     } else if card_status & ADDRESS_OUT_OF_RANGE.bits() != 0 {
-        return Err(Error::CardError { t: ADDRESS_OUT_OF_RANGE });
+        Err(Error::CardError { t: ADDRESS_OUT_OF_RANGE })
     } else {
-        return Err(Error::CardError { t: ERROR });
+        Err(Error::CardError { t: ERROR })
     }
 }
--- src/sd/init.rs
+++ src/sd/init.rs
@@ -4,11 +4,22 @@ use board::rcc::Rcc;
 use board::sdmmc::Sdmmc;
 use embedded::interfaces::gpio::Gpio;
 
+/// Initializes the SD card. This includes the initialization of the pins and clocks that the card
+/// needs to operate. If the initialization succeeds an SD card struct is returned.
+///
+/// # Errors
+///
+/// The initialization returns an error if something in the process went wrong. Most of the time
+/// this should only be the Error `NoSdCard`. Other errors are an indication that some clock or pin
+/// could not be activated correctly.
 pub fn init(sdmmc: &'static mut Sdmmc, gpio: &mut Gpio, rcc: &mut Rcc) -> Result<Sd, Error> {
+    // Check for SD card
+    sd_card_present(gpio, rcc)?;
+
+    // Initialize clock and pins
     init_hw(gpio, rcc)?;
 
     // default clock configuration
-    // TODO: hardcoded
     sdmmc.clkcr.update(|clkcr| {
         clkcr.set_negedge(false);
         clkcr.set_bypass(false);
@@ -20,33 +31,48 @@ pub fn init(sdmmc: &'static mut Sdmmc, gpio: &mut Gpio, rcc: &mut Rcc) -> Result
 
     let card_type = power_on(sdmmc)?;
 
+    command::send_cmd_send_cid(sdmmc)?;
+
+    let rca = command::send_cmd_set_rel_add(sdmmc)?;
+
     Ok(Sd {
-        _registers: sdmmc,
-        _card_type: card_type,
+        _sdmmc: sdmmc,
+        card_type: card_type,
+        rca: rca,
     })
 }
 
-fn init_hw(gpio: &mut Gpio, rcc: &mut Rcc) -> Result<(), Error> {
+fn sd_card_present(gpio: &mut Gpio, rcc: &mut Rcc) -> Result<(), Error> {
     use embedded::interfaces::gpio::Port::*;
     use embedded::interfaces::gpio::Pin::*;
     use embedded::interfaces::gpio::Resistor;
 
-    rcc.ahb1enr.update(|r| {
-        r.set_gpiocen(true); // Data and clock port
-        r.set_gpioden(true); // CMD port
-        r.set_gpioben(true); // TODO: only needed in mmc 8bit mode
-    });
+    rcc.ahb1enr.update(|r| r.set_gpiocen(true));
     // wait for enabling
-    while rcc.ahb1enr.read().gpiocen()
-            && rcc.ahb1enr.read().gpioden()
-            && rcc.ahb1enr.read().gpioben() {}
+    while !rcc.ahb1enr.read().gpiocen() {}
 
-    // Check if a SDcard is present
     let sd_not_present = gpio.to_input((PortC, Pin13), Resistor::PullUp).unwrap();
-
     if sd_not_present.get() {
         return Err(Error::NoSdCard);
     }
+
+    Ok(())
+}
+
+fn init_hw(gpio: &mut Gpio, rcc: &mut Rcc) -> Result<(), Error> {
+    // Enable SDMMC1 clock
+    rcc.apb2enr.update(|r| r.set_sdmmc1en(true));
+    // Enable data and command port
+    rcc.ahb1enr.update(|r| {
+        r.set_gpiocen(true); // Data and clock port
+        r.set_gpioden(true); // CMD port
+        // r.set_gpioben(true); // only needed in mmc 8bit mode
+    });
+    // wait for enabling
+    while !rcc.apb2enr.read().sdmmc1en()
+            || !rcc.ahb1enr.read().gpiocen()
+            || !rcc.ahb1enr.read().gpioden() {}
+            // || !rcc.ahb1enr.read().gpioben() {}
 
     init_pins(gpio);
 
@@ -58,6 +84,7 @@ fn init_pins(gpio: &mut Gpio) {
     use embedded::interfaces::gpio::Pin::*;
     use embedded::interfaces::gpio::{AlternateFunction, OutputType, OutputSpeed, Resistor};
 
+    // Data ports. For Default Bus mode only d0 is needed.
     let d0 = (PortC, Pin8);
     let d1 = (PortC, Pin9);
     let d2 = (PortC, Pin10);
@@ -67,8 +94,10 @@ fn init_pins(gpio: &mut Gpio) {
     let d6 = (PortC, Pin6);
     let d7 = (PortC, Pin7);
 
+    // Clock
     let ck = (PortC, Pin12);
 
+    // Command
     let cmd = (PortD, Pin2);
 
     let pins = [d0,
@@ -87,20 +116,66 @@ fn init_pins(gpio: &mut Gpio) {
                                    OutputType::PushPull,
                                    OutputSpeed::High,
                                    Resistor::PullUp).unwrap();
-
 }
 
 fn power_on(sdmmc: &mut Sdmmc) -> Result<CardType, Error> {
+    // power up the card
     sdmmc.clkcr.update(|clkcr| clkcr.set_clken(false));
     sdmmc.power.update(|pwr| pwr.set_pwrctrl(0x03));
     sdmmc.clkcr.update(|clkcr| clkcr.set_clken(true));
 
     let mut card_type = CardType::SDv1;
+
     // set sd card to idle state
-    command::send_command_idle(sdmmc, 5000)?;
-    if let Ok(_) = command::send_command_oper_cond(sdmmc) {
-        card_type = CardType::SDv2;
-    };
+    command::send_cmd_idle(sdmmc, 5000)?;
+
+    // get Card version and operation voltage
+    let mut count = 0;
+    let max_volt_trial = 0xFFFF;
+    let mut valid_voltage = false;
+    if command::send_cmd_oper_cond(sdmmc).is_ok() {
+        let mut card_status = 0;
+        // voltage trial for card V2
+        while !valid_voltage {
+            if count == max_volt_trial {
+                return Err(Error::InvalidVoltrange)
+            }
+            count += 1;
+
+            // Send CMD55, needed for next CMD. println!("Send CMD55");
+            command::send_cmd_app(sdmmc, 0)?;
+
+            // Send ACMD41. 0x40..0 for high capacity. println!("Send ACMD41");
+            command::send_cmd_app_oper(sdmmc, 0x4000_0000)?;
+
+            card_status = sdmmc.resp1.read().cardstatus1();
+
+            valid_voltage = card_status >> 31 == 1
+        }
+        // determine whether high or standard capacity.
+        if card_status & 0x4000_0000 != 0 {
+            card_type = CardType::SDv2HC;
+        } else {
+            card_type = CardType::SDv2SC;
+        }
+    } else {
+        while !valid_voltage {
+            if count == max_volt_trial {
+                return Err(Error::InvalidVoltrange)
+            }
+            count += 1;
+
+            // Send CMD55, needed for next CMD. println!("Send CMD55");
+            command::send_cmd_app(sdmmc, 0)?;
+
+            // Send ACMD41. 0x0 for standard capacity. println!("Send ACMD41");
+            command::send_cmd_app_oper(sdmmc, 0x0)?;
+
+            let card_status = sdmmc.resp1.read().cardstatus1();
+
+            valid_voltage = card_status >> 31 == 1
+        }
+    }
 
     Ok(card_type)
 }
