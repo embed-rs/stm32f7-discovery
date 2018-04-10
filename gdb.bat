777e843222608c9d98254e5bcf863b723aa46d68
Fabio B <f-bro@mailbox.org>
2018-04-10 11:52:27 +0200

    Simplified gdb.bat

--- gdb.bat
+++ gdb.bat
@@ -1,4 +1,3 @@
 echo "Please run 'st-util' in another terminal window"
 echo ""
-set path=%1
-arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" %path%
+arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" %1
