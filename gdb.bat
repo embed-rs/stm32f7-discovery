65d92a7017060c33298d27f4c665d92501c22251
Fabio B <f-bro@mailbox.org>
2018-04-10 11:03:11 +0200

    Set target runner for windows

--- gdb.bat
+++ gdb.bat
@@ -1,4 +1,4 @@
 echo "Please run 'st-util' in another terminal window"
 echo ""
-
-arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" target/stm32f7/debug/stm32f7_discovery
+set path=%1
+arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" %path%
