19af8c3dedfc217b1a2e35c5bf9dca839b0a94cc
Philipp Oppermann <dev@phil-opp.com>
2018-06-01 14:59:33 +0200

    Proxy gdb invocation through script to set up auto-load-safe-path

---
+++ gdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Please run 'st-util' in another terminal window (you might need sudo)"
+echo ""
+
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    # Linux*)     machine=Linux;;
+    Darwin*)    arm-none-eabi-gdb-py -iex 'add-auto-load-safe-path .' $1;;
+    *)          arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' $1
+esac
