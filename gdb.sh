da214ddf6a333c4a4abd9e48ef5e47e85756e4a5
Philipp Oppermann <dev@phil-opp.com>
2018-06-10 17:12:54 +0200

    Update gdb message to use openocd instead of stlink

--- gdb.sh
+++ gdb.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Please run 'st-util' in another terminal window (you might need sudo)"
+echo "Please run openocd in another terminal window (you might need sudo)"
 echo ""
 
 # https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
