d6a125168e28ce6cc2bebca89658afc73234df42
Philipp Oppermann <dev@phil-opp.com>
2018-10-12 15:54:58 +0200

    Port blinking_led example to rewrite

---
+++ examples/blinking_led.rs
@@ -0,0 +1,97 @@
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+
+extern crate stm32f7_discovery;
+extern crate cortex_m_rt as rt;
+extern crate stm32f7;
+extern crate alloc_cortex_m;
+extern crate cortex_m;
+extern crate cortex_m_semihosting;
+
+use stm32f7::stm32f7x6::{CorePeripherals, Peripherals};
+use stm32f7_discovery::{
+    init,
+    system_clock::{self, Hz},
+    gpio::{GpioPort, OutputPin},
+};
+use rt::{entry, exception};
+use core::alloc::Layout as AllocLayout;
+use core::panic::PanicInfo;
+use alloc_cortex_m::CortexMHeap;
+
+#[entry]
+fn main() -> ! {
+    let core_peripherals = CorePeripherals::take().unwrap();
+    let mut systick = core_peripherals.SYST;
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut rcc = peripherals.RCC;
+    let mut pwr = peripherals.PWR;
+    let mut flash = peripherals.FLASH;
+
+    init::init_system_clock_216mhz(&mut rcc, &mut pwr, &mut flash);
+    init::enable_gpio_ports(&mut rcc);
+
+    let gpio_a = GpioPort::new_a(&peripherals.GPIOA);
+    let gpio_b = GpioPort::new_b(&peripherals.GPIOB);
+    let gpio_c = GpioPort::new(&peripherals.GPIOC);
+    let gpio_d = GpioPort::new(&peripherals.GPIOD);
+    let gpio_e = GpioPort::new(&peripherals.GPIOE);
+    let gpio_f = GpioPort::new(&peripherals.GPIOF);
+    let gpio_g = GpioPort::new(&peripherals.GPIOG);
+    let gpio_h = GpioPort::new(&peripherals.GPIOH);
+    let gpio_i = GpioPort::new(&peripherals.GPIOI);
+    let gpio_j = GpioPort::new(&peripherals.GPIOJ);
+    let gpio_k = GpioPort::new(&peripherals.GPIOK);
+    let mut pins = init::pins(
+        gpio_a, gpio_b, gpio_c, gpio_d, gpio_e, gpio_f, gpio_g, gpio_h, gpio_i, gpio_j, gpio_k,
+    );
+
+    // configure the systick timer 20Hz (20 ticks per second)
+    init::init_systick(Hz(20), &mut systick, &rcc);
+    systick.enable_interrupt();
+
+    // turn led on
+    pins.led.set(true);
+
+    let mut last_led_toggle = system_clock::ticks();
+    loop {
+        let ticks = system_clock::ticks();
+        // every 0.5 seconds (we have 20 ticks per second)
+        if ticks - last_led_toggle >= 10 {
+            pins.led.toggle();
+            last_led_toggle = ticks;
+        }
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+#[exception]
+fn SysTick() {
+    system_clock::tick();
+}
+
+// define what happens in an Out Of Memory (OOM) condition
+#[alloc_error_handler]
+fn rust_oom(_: AllocLayout) -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    use core::fmt::Write;
+    use cortex_m_semihosting::hio;
+    use cortex_m::{asm};
+
+    if let Ok(mut hstdout) = hio::hstdout() {
+        let _ = writeln!(hstdout, "{}", info);
+    }
+
+    // OK to fire a breakpoint here because we know the microcontroller is connected to a debugger
+    asm::bkpt();
+    
+    loop {}
+}
