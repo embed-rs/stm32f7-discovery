1c317c8b9eef5016672af1fc01e03e5c145b5127
Philipp Oppermann <dev@phil-opp.com>
2018-11-12 13:01:30 +0100

    Fix deadlock by using mpsc channel instead of Mutex<Vec>

--- src/task_runtime.rs
+++ src/task_runtime.rs
@@ -1,4 +1,5 @@
 use core::pin::Pin;
+use core::ops::{Add, AddAssign};
 use alloc::{
     prelude::*,
     sync::Arc,
@@ -8,15 +9,16 @@ use alloc::{
 use futures::{
     prelude::*,
     future::{FutureObj, LocalFutureObj, UnsafeFutureObj},
-    task::{Spawn, LocalSpawn, SpawnError},
+    task::{Poll, Spawn, LocalSpawn, SpawnError},
+    channel::mpsc,
 };
-use spin::Mutex;
 
 pub struct Executor {
-    tasks: BTreeMap<u64, Pin<Box<LocalFutureObj<'static, ()>>>>,
-    ready_tasks: Vec<u64>,
-    woken_tasks: Arc<Mutex<Vec<u64>>>,
-    next_task_id: u64,
+    tasks: BTreeMap<TaskId, Pin<Box<LocalFutureObj<'static, ()>>>>,
+    ready_tasks: Vec<TaskId>,
+    woken_tasks: mpsc::UnboundedReceiver<TaskId>,
+    woken_tasks_sender: mpsc::UnboundedSender<TaskId>,
+    next_task_id: TaskId,
 }
 
 impl Spawn for Executor {
@@ -34,11 +36,13 @@ impl LocalSpawn for Executor {
 
 impl Executor {
     pub fn new() -> Self {
+        let (sender, receiver) = mpsc::unbounded();
         Executor {
             tasks: BTreeMap::new(),
             ready_tasks: Vec::new(),
-            woken_tasks: Arc::new(Mutex::new(Vec::new())),
-            next_task_id: 0,
+            woken_tasks: receiver,
+            woken_tasks_sender: sender,
+            next_task_id: TaskId(0),
         }
     }
 
@@ -50,35 +54,50 @@ impl Executor {
     }
 
     pub fn run(&mut self) {
-        {
-            let mut woken_tasks = self.woken_tasks.lock();
-            for task_id in woken_tasks.drain(..) {
-                self.ready_tasks.push(task_id);
-            }
+        while let Ok(task_id) = self.woken_tasks.try_next() {
+            let task_id = task_id.expect("woken_tasks stream has terminated");
+            self.ready_tasks.push(task_id);
         }
         for task_id in self.ready_tasks.drain(..) {
             let waker = MyWaker {
                 task_id,
-                woken_tasks: self.woken_tasks.clone(),
+                woken_tasks: self.woken_tasks_sender.clone(),
             };
             let poll_result = {
-                let task = self.tasks.get_mut(&task_id).expect(&format!("task with id {} not found", task_id));
+                let task = self.tasks.get_mut(&task_id).expect(&format!("task with id {:?} not found", task_id));
                 task.as_mut().poll(&local_waker_from_nonlocal(Arc::new(waker)))
             };
             if poll_result.is_ready() {
-                self.tasks.remove(&task_id).expect(&format!("Task {} not found", task_id));
+                self.tasks.remove(&task_id).expect(&format!("Task {:?} not found", task_id));
             }
         };
     }
 }
 
 struct MyWaker {
-    task_id: u64,
-    woken_tasks: Arc<Mutex<Vec<u64>>>,
+    task_id: TaskId,
+    woken_tasks: mpsc::UnboundedSender<TaskId>,
 }
 
 impl Wake for MyWaker {
     fn wake(arc_self: &Arc<Self>) {
-        arc_self.woken_tasks.lock().push(arc_self.task_id);
+        arc_self.woken_tasks.unbounded_send(arc_self.task_id);
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl Add<u64> for TaskId {
+    type Output = TaskId;
+
+    fn add(self, other: u64) -> TaskId {
+        TaskId(self.0 + other)
+    }
+}
+
+impl AddAssign<u64> for TaskId {
+    fn add_assign(&mut self, other: u64) {
+        self.0 += other;
     }
 }
